d80c4c8d1549fc75172204ece8bad31d964ca39e
twarling <warli010@morris.umn.edu>
2017-08-30 20:15:42 -0500

    created a tar archive of the undeleted files and added a prefix of cleaned_ to the archive's filename

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,6 +3,8 @@
 tar=$1
 dir=$2
 
+mainDir=$(pwd)
+
 tar -xzf $tar -C $dir
 
 cd $dir/*
@@ -10,3 +12,7 @@ cd $dir/*
 for i in $(pwd)/*; do	
 	 grep -l "DELETE ME!" $i | xargs rm -f
 done
+
+tar -zcf  cleaned_$tar *
+
+cd $mainDir
